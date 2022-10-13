3e57e4b9715690e539b48bfc252b76831a4348e9
Luiz Claudio de Holanda <luiz.holanda@visionaespacial.com.br>
2022-10-13 10:29:57 -0300

    Change main

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "net/http"
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello world from GO!"))
+		w.Write([]byte("Hello world from GO!!!!!"))
 	})
 	http.ListenAndServe(":9000", nil)
 
