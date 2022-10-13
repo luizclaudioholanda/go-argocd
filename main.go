04d45144a33ebfa9a41c3a9548d293ed6ab06905
Luiz Claudio de Holanda <luiz.holanda@visionaespacial.com.br>
2022-10-13 09:13:36 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world from GO!"))
+	})
+	http.ListenAndServe(":9000", nil)
+
+}
